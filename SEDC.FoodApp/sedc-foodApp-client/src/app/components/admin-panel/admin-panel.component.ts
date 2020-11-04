12e9f6e7b31ebd65f0619602d9f79df0b17d26de
Viktor Jakovlev <vicebad@hotmail.com>
2020-11-04 19:31:04 +0100

    add restaurant feature added

--- SEDC.FoodApp/sedc-foodApp-client/src/app/components/admin-panel/admin-panel.component.ts
+++ SEDC.FoodApp/sedc-foodApp-client/src/app/components/admin-panel/admin-panel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Municipality, RestaurantRequestModel } from 'src/app/models/restaurant-model';
 import { AdminPanelService } from 'src/app/services/admin-panel.service';
@@ -16,7 +16,7 @@ export class AdminPanelComponent implements OnInit {
   municipalityList = [Municipality.karpos, Municipality.centar, Municipality.aerodrom]
 
   requestForm = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', Validators.required),
     address: new FormControl(''),
     municipality: new FormControl('')
   })
@@ -28,6 +28,9 @@ export class AdminPanelComponent implements OnInit {
 
   addRestaurant() {
     let requestModel = new RestaurantRequestModel();
+    requestModel.name = this.requestForm.value.name
+    requestModel.address = this.requestForm.value.address
+    requestModel.municipality = parseInt(this.requestForm.value.municipality)
 
     this.adminPanelService.addRestaurant(requestModel).subscribe({
       error: err => console.warn(err.error)
@@ -36,6 +39,12 @@ export class AdminPanelComponent implements OnInit {
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
+  }
+
+  closeModal() {
+    this.modalService._hideModal()
+    this.modalService._hideBackdrop()
+
   }
 
 }
